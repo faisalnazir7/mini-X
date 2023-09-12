7ac1cfcf837a573f096c3104f4dcc1b36fcada1c
Faisal Nazir <bhatfaisalfnb@gmail.com>
2023-09-13 00:19:09 +0530

    userModel

---
+++ Server/models/userModel.ts
@@ -0,0 +1,58 @@
+import mongoose, { Document, Schema, Types } from "mongoose";
+
+// Define the User schema
+export interface IUser extends Document {
+  email: string;
+  name: string;
+  userName: string;
+  password: string;
+  photo?: string;
+  followers: Types.ObjectId[];
+  following: Types.ObjectId[];
+}
+
+const userSchema = new Schema<IUser>({
+  email: {
+    type: String,
+    required: [true, "Please add a email"],
+    unique: true,
+    trim: true,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please enter a valid email",
+    ],
+  },
+  name: {
+    type: String,
+    required: [true, "Please add a name"],
+  },
+  userName: {
+    type: String,
+    required: [true, "Please add a username"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please add a password"],
+  },
+  photo: {
+    type: String,
+    default: "https://i.ibb.co/4pDNDk1/avatar.png",
+  },
+  followers: [
+    {
+      type: Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  following: [
+    {
+      type: Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+// Define and export the User model
+const User = mongoose.model<IUser>("User", userSchema);
+
+export default User;
