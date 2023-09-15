db15428dba582bb8a70d16c6ee86789ef845f7f7
Faisal Nazir <bhatfaisalfnb@gmail.com>
2023-09-15 15:14:28 +0530

    Sign Up page and modify>>signin

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
+import Signup from './Pages/SignUp/SignUp';
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
   );
 }
--- Client/src/Pages/SignUp/SignUp.tsx
+++ Client/src/Pages/SignUp/SignUp.tsx
@@ -2,20 +2,24 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Logo from '../../assets/x-logo.png';
 
-function Login() {
+function Signup() {
   const navigate = useNavigate();
+  const [name, setName] = useState('');
   const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
 
-  const loginUser = () => {
-    fetch(`${import.meta.env.VITE_SERVER_URL}/api/users/login`, {
+  const signupUser = () => {
+    fetch(`${import.meta.env.VITE_SERVER_URL}/api/users/register`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         userName: userName,
+        name: name,
+        email: email,
         password: password,
       }),
     })
@@ -29,21 +33,21 @@ function Login() {
           navigate('/');
         }
       });
-  }
+  };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div>
           <img className="mx-auto h-12 w-auto" src={Logo} alt="Logo" />
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Log in to your account</h2>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign up for a new account</h2>
         </div>
         {message && (
           <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4" role="alert">
             <p>{message}</p>
           </div>
         )}
-        <form className="mt-8 space-y-6" onSubmit={loginUser}>
+        <form className="mt-8 space-y-6" onSubmit={signupUser}>
           <div className="rounded-md shadow-sm -space-y-px">
             <div>
               <label htmlFor="userName" className="sr-only">
@@ -61,6 +65,36 @@ function Login() {
               />
             </div>
             <div>
+              <label htmlFor="userName" className="sr-only">
+                Full Name
+              </label>
+              <input
+                id="name"
+                name="name"
+                type="text"
+                autoComplete="name"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Full Name"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="email" className="sr-only">
+                Email address
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Email address"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
               <label htmlFor="password" className="sr-only">
                 Password
               </label>
@@ -68,7 +102,7 @@ function Login() {
                 id="password"
                 name="password"
                 type="password"
-                autoComplete="current-password"
+                autoComplete="new-password"
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Password"
@@ -80,16 +114,16 @@ function Login() {
           <div>
             <button
               type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-full text-white bg-sky-500 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-full text-white bg-sky-500 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
-              Log in
+              Sign Up
             </button>
           </div>
         </form>
         <p className="mt-2 text-center text-sm text-gray-600">
-          Don't have an account?{' '}
-          <Link to="/signup" className="font-medium text-sky-600 hover:text-sky-500">
-            Sign up
+          Already have an account?{' '}
+          <Link to="/login" className="font-medium text-sky-600 hover:text-sky-500">
+            Log in
           </Link>
         </p>
       </div>
@@ -97,4 +131,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default Signup;
